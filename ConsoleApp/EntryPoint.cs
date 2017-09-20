74ff2b11a7e8538e6f31b0822951bb83a16460a2
Borovsky Arkady <borovskyav@list.ru>
2017-09-20 21:15:44 +0500

    Initial commit

---
+++ ConsoleApp/EntryPoint.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using TextJustifier.Core;
+
+namespace TextJustifier.ConsoleApp
+{
+    class EntryPoint
+    {
+        static void Main()
+        {
+            var justifier = new Justifier();
+            Console.WriteLine(justifier.Justify("a b c", 5));
+            Console.Read();
+        }
+    }
+}
---
+++ Tests/JustifierTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using TextJustifier.Core;
+
+namespace TextJustifier.Tests
+{
+    [TestFixture]
+    public class JustifierTests
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            Justifier = new Justifier();
+        }
+
+        [Test]
+        public void Test()
+        {
+            Assert.AreEqual(Justifier.Justify("a b c", 5), "a b c");
+        }
+
+        private Justifier Justifier { get; set; }
+    }
+}
