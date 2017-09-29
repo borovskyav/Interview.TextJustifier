552df0595ec54c46579f26c0844e0aaad5e23be7
ffominyh <ffominyh@skbkontur.ru>
2017-09-29 14:46:06 +0500

    add project with mstests

---
+++ Tests.ms/JustifierAdditionalConditionsTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Reflection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TextJustifier.Core;
+
+namespace TextJustifier.Tests
+{
+    [TestClass]
+    public class JustifierAdditionalConditionsTests
+    {
+        private Justifier Justifier { get; set; }
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            Justifier = new Justifier();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestWordsLongerThanGivenLength_1()
+        {
+            Justifier.Justify("Hello", 4);
+        }
+
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestWordsLongerThanGivenLength_2()
+        {
+            Justifier.Justify("CSharp is a programming language", 10);
+        }
+
+
+        [TestMethod]
+        public void TestNewlines()
+        {
+            Assert.AreEqual("I  don't  like\nblack   coffee", Justifier.Justify("I don't like black coffee", 14));
+            Assert.AreEqual("a  b\nc  d", Justifier.Justify("a b c d", 4));
+            Assert.AreEqual("She   does\nnot   play", Justifier.Justify("She does not play", 10));
+        }
+
+        [TestMethod]
+        public void TestOneWordInNewlines()
+        {
+            Assert.AreEqual("I   play\ngames   ", Justifier.Justify("I play games", 8));
+            Assert.AreEqual("I     am\nplaying \na   game", Justifier.Justify("I am playing a game", 8));
+        }
+    }
+}
---
+++ Tests.ms/JustifierTests.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TextJustifier.Core;
+
+namespace TextJustifier.Tests
+{
+    [TestClass]
+    public class JustifierTests
+    {
+        [TestInitialize]
+        public void SetUp()
+        {
+            Justifier = new Justifier();
+        }
+
+        [TestMethod]
+        public void TestSimpleUsage()
+        {
+            Assert.AreEqual("a b c d e", Justifier.Justify("a b c d e", 9));
+            Assert.AreEqual("I play games", Justifier.Justify("I play games", 12));
+            Assert.AreEqual("I came here to see you", Justifier.Justify("I came here to see you", 22));
+            Assert.AreEqual("Java was invented by Sun", Justifier.Justify("Java was invented by Sun", 24));
+        }
+
+        [TestMethod]
+        public void TestOneWord()
+        {
+            Assert.AreEqual("I        ", Justifier.Justify("I", 9));
+            Assert.AreEqual("Like  ", Justifier.Justify("Like", 6));
+            Assert.AreEqual("CSharp", Justifier.Justify("CSharp", 6));
+        }
+
+        [TestMethod]
+        public void TestSameNumberOfSpaces()
+        {
+            Assert.AreEqual("a  b  c", Justifier.Justify("a b c", 7));
+            Assert.AreEqual("a   b   c", Justifier.Justify("a b c", 9));
+            Assert.AreEqual("I  play  games", Justifier.Justify("I play games", 14));
+        }
+
+        [TestMethod]
+        public void TestDifferentNumberOfSpaces()
+        {
+            Assert.AreEqual("a  b c", Justifier.Justify("a b c", 6));
+            Assert.AreEqual("I   play  games", Justifier.Justify("I play games", 15));
+            Assert.AreEqual("I   do   not  play", Justifier.Justify("I do not play", 18));
+        }
+
+        private Justifier Justifier { get; set; }
+    }
+}
